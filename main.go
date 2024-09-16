b5afca11c1a0113e14b3e3ebf9b98c19a8fe69d3
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-09-16 11:06:31 +0200

    make styles global + remove base.html + update handlers

--- main.go
+++ main.go
@@ -31,81 +31,81 @@ func main() {
 
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 
+	http.HandleFunc("/", handle404)
+	http.HandleFunc("/{$}", handleIndex)
+	http.HandleFunc("/blogs/{$}", handleBlogs)
+	http.HandleFunc("/blogs/{id}", handleBlogId)
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			handle404(w)
-			return
-		}
-
-		resp, err := http.Get("http://localhost:8080/v1/blogs")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		var blogs []blog
-
-		if err := json.NewDecoder(resp.Body).Decode(&blogs); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		templates.ExecuteTemplate(w, "index.html", map[string]any{
-			"Blogs": blogs,
-		})
-	})
-
-	http.HandleFunc("/blogs", func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("http://localhost:8080/v1/blogs")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		var blogs []blog
-
-		if err := json.NewDecoder(resp.Body).Decode(&blogs); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		templates.ExecuteTemplate(w, "blogs.html", map[string]any{
-			"Blogs": blogs,
-		})
-	})
-
-	http.HandleFunc("/blogs/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-
-		resp, err := http.Get("http://localhost:8080/v1/blogs/" + id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode == 404 {
-			handle404(w)
-			return
-		}
-
-		var blog blog
-		if err = json.NewDecoder(resp.Body).Decode(&blog); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		templates.ExecuteTemplate(w, "blog.html", blog)
-	})
 
 	log.Println("app listening on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func handle404(w http.ResponseWriter) {
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://localhost:8080/v1/blogs")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	var blogs []blog
+
+	if err := json.NewDecoder(resp.Body).Decode(&blogs); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	templates.ExecuteTemplate(w, "home.html", map[string]any{
+		"Blogs": blogs,
+	})
+}
+
+func handleBlogs(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://localhost:8080/v1/blogs")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	var blogs []blog
+
+	if err := json.NewDecoder(resp.Body).Decode(&blogs); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	templates.ExecuteTemplate(w, "blogs.html", map[string]any{
+		"Blogs": blogs,
+	})
+}
+
+func handleBlogId(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	resp, err := http.Get("http://localhost:8080/v1/blogs/" + id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		handle404(w, r)
+		return
+	}
+
+	var blog blog
+	if err = json.NewDecoder(resp.Body).Decode(&blog); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	templates.ExecuteTemplate(w, "blog.html", blog)
+}
+
+func handle404(w http.ResponseWriter, _ *http.Request) {
 	templates.ExecuteTemplate(w, "404.html", nil)
 }
